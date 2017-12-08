3db6cc26b5af13b99bc05887214b3082ce1e7b40
Gregory Paik <gregorypaik@Gregorys-MacBook-Pro.local>
2017-12-07 23:36:42 -0800

    database for actor names

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
-
-// Tells node that we are creating an "express" server
-var app = express();
-
-// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
-// BodyParser makes it easy for our server to interpret data sent to it.
-// The code below is pretty standard.
+var db = require("./models");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-require("./routes/api-Routes")(app);
-require("./routes/html-Routes")(app);
+app.use(express.static("public"));
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+require("./routes/api-routes.js")(app);
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
