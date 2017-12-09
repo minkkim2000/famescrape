1e549bb7f29232af225b15886f472ec252f04660
Sungwoo Lee <sungwoo.d.lee@gmail.com>
2017-12-09 11:17:02 -0800

    save edit

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,24 +4,29 @@ module.exports = function(app) {
     // Search for Specific celeb (or all celebs) then provides JSON
     app.get("/api/:celebs?", function(req, res) {
         // Then display the JSON for ONLY that celeb.
-        db.Actor.findOne({
+        if db.Actor.findOne({
             where: {
-                routeName: req.params.celebs
+                name: req.params.name
             }
         }).then(function(dbActor) {
             return res.json(dbActor);
         });
+
+        // Otherwise...
+        else {
+            app.use(function (err, req) {
+              console.error(err.stack)
+              res.status(500).send("Please search new actor/actress")
+            })
+        }
     });
 
     // POST route for saving a new todo
     app.post("/api/celebs", function(req, res) {
-        var celeb = req.body;
-        var routeName = celeb.name.replace(/\s+/g, "").toLowerCase();
         // create takes an argument of an object describing the item we want to
         // insert into our table. In this case we just we pass in an object with a text
         // and complete property
         db.Actor.create({
-            routeName: routeName,
             name: req.body.name,
             valid: req.body.valid
         }).then(function(dbActor) {
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,6 +8,7 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
+  
 
   app.get("/profile", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/profile.html"));
